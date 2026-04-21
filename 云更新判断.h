417ec5961c58f87f6cf9bdf8cb30d17f6808ed1e
beiyuannb666 <beiyuan_01@qq.com>
2026-04-22 02:21:52 +0800

    Add files via upload

--- 云更新判断.h
+++ 云更新判断.h
@@ -1,6 +1,6 @@
 [版本号]2[版本号]
 [公告]官方频道ZEONB669[公告]
-[文件名]ZEO半火美化.sh[文件名]
+[文件名][文件名]
 [链接]
-https://raw.githubusercontent.com/beiyuannb666/-/main/ZEO半火美化.sh
+https://github.com/beiyuannb666/-/raw/refs/heads/main/ZEO%E5%8D%8A%E7%81%AB%E7%BE%8E%E5%8C%96.sh
 [链接]

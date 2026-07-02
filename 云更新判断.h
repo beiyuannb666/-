3a81411ab41c648a25d425943dea1e488b0bbcc2
beiyuannb666 <beiyuan_01@qq.com>
2026-07-03 02:12:41 +0800

    Add files via upload

--- 云更新判断.h
+++ 云更新判断.h
@@ -1,5 +1,5 @@
-[版本号]1.01[版本号]
+[版本号]1.02[版本号]
 [公告]官方频道ZEONB669
 公益卡密;ZEONB666C32C0E46[公告]
 [文件名]ZEO-PUBG[衍生版].sh[文件名]
-[链接]https://github.com/beiyuannb666/-/raw/refs/heads/main/ZEO-PUBG%5B%E8%A1%8D%E7%94%9F%E7%89%88%5D.sh[链接]
+[链接]http://wpan.cdndns.site/down/ZEONHNB[链接]
